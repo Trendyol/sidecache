e6b0a442c964d38cd5feae281b640e3a084f7ae6
Haluk Emre Savcı <emre.savci@trendyol.com>
2020-04-28 02:00:15 +0300

    - refactor

--- cmd/sidecache/main.go
+++ cmd/sidecache/main.go
@@ -1,279 +1,47 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"net"
-	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"strconv"
-	"strings"
-	"time"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/Trendyol/sidecache/pkg/cache"
+	"github.com/Trendyol/sidecache/pkg/server"
+	"go.uber.org/zap"
 )
 
-type CacheRequest struct {
-	CacheKey string `json: "cacheKey"`
-	Data     []byte `json: "data"`
-	TTL      int    `json: "ttl"`
-}
-
-func startHTTPServer() {
-	srv := &http.Server{Addr: ":9090"}
-	repo := cache.NewRedisRepository()
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "POST" {
-			var request CacheRequest
-			requestBody, err := ioutil.ReadAll(r.Body)
-
-			if err != nil {
-				fmt.Println(err)
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			json.Unmarshal(requestBody, &request)
-			repo.SetKeyTTL(request.CacheKey, request.Data, request.TTL)
-		} else if r.Method == "GET" {
-			w.Header().Set("Content-Type", "application/json")
-
-			key := r.URL.Query().Get("cacheKey")
-			data := repo.Get(key)
-
-			w.Write(data)
-		}
+var counter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "sidecache",
+		Name:      "cache_hit_counter",
+		Help:      "This is my counter",
 	})
 
-	go func() {
-		if err := srv.ListenAndServe(); err != nil {
-			fmt.Printf("Httpserver: ListenAndServe() error: %s \n", err)
-		}
-	}()
-
-	<-make(chan int, 0)
-}
-
-var httpResponseStart []byte
-var repo *cache.CouchbaseRepository
-var prxy *httputil.ReverseProxy
+var gauge = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Namespace: "sidecache",
+		Name:      "cache_hit",
+		Help:      "This is cache hit metric",
+	})
 
 func main() {
-	repo = cache.NewCouchbaseRepository()
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
 
-	//mainContainerPort := os.Getenv("MAIN_CONTAINER_PORT")
-	url, err := url.Parse("http://127.0.0.1:8080")
+	prometheus.MustRegister(counter)
+
+	couchbaseRepo := cache.NewCouchbaseRepository()
+
+	mainContainerPort := os.Getenv("MAIN_CONTAINER_PORT")
+	mainContainerURL, err := url.Parse("http://127.0.0.1:" + mainContainerPort)
 	if err != nil {
-		fmt.Println(err)
+		logger.Error("Error occured on url.Parse", zap.Error(err))
 	}
 
-	fmt.Println(url == nil)
-	fmt.Println(repo == nil)
-	fmt.Println(url)
-	fmt.Println(repo)
+	proxy := httputil.NewSingleHostReverseProxy(mainContainerURL)
+	cacheServer := server.NewServer(couchbaseRepo, proxy, counter, logger)
 
-	prxy = httputil.NewSingleHostReverseProxy(url)
-
-	prxy.ModifyResponse = func(r *http.Response) error {
-		defer elapsed("ModifyResponse")()
-		//if r.Header.Get("Cache-TTL") == "300" {
-		fmt.Println("modify response")
-		b, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			return err
-		}
-
-		go func(url string, data []byte) {
-			hashedURL := hashURL(url)
-			repo.SetKey(hashedURL, data)
-		}(r.Request.URL.RequestURI(), b)
-
-		err = r.Body.Close()
-		if err != nil {
-			return err
-		}
-
-		body := ioutil.NopCloser(bytes.NewReader(b))
-		r.Body = body
-		//}
-
-		return nil
-	}
-
-	http.HandleFunc("/", CacheHandler)
-
-	log.Fatal(http.ListenAndServe(":9191", nil))
-
-	/*
-		// TODO: only get requests
-
-		httpResponseStart = []byte("HTTP/1.1 200 OK\nContent-Type: application/json;\nCustom-CacheProxy:True\nContent-Length:")
-
-		listener, err := net.Listen("tcp", ":9191")
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(0)
-		}
-		defer listener.Close()
-
-		for {
-			conn, err := listener.Accept()
-
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				go func() {
-
-					// request al url extract et
-					//
-					var buffer bytes.Buffer
-					teeReader := io.TeeReader(conn, &buffer)
-
-					url := readURL(teeReader)
-					hashedURL := hashURL(url)
-					cachedData := checkCache(hashedURL)
-
-					fmt.Println(hashedURL)
-
-					if cachedData == nil {
-						dataChannel := make(chan ([]byte))
-						proxy(conn, &buffer, dataChannel)
-
-						go func(ch chan ([]byte), url string) {
-							data := <-ch
-							repo.SetKey(url, data)
-						}(dataChannel, hashedURL)
-					} else {
-						fmt.Println("data cachede bulundu")
-						returnResponse(cachedData, conn)
-					}
-				}()
-			}
-		}
-	*/
-
-}
-
-func elapsed(methodName string) func() {
-	start := time.Now()
-	return func() {
-		fmt.Printf("%s took %v\n", methodName, time.Since(start))
-	}
-}
-
-func CacheHandler(w http.ResponseWriter, r *http.Request) {
-	defer elapsed("CacheHandler")() // <-- The trailing () is the deferred call
-
-	defer func() {
-		if rec := recover(); rec != nil {
-			fmt.Printf("Recovered from panic: %v \n", rec)
-			var err error
-			switch x := rec.(type) {
-			case string:
-				err = errors.New(x)
-			case error:
-				err = x
-			default:
-				err = errors.New("Unknown panic")
-			}
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	}()
-
-	hashedURL := hashURL(r.RequestURI)
-	cachedData := checkCache(hashedURL)
-	if cachedData != nil {
-		fmt.Println("cache found")
-		io.Copy(w, bytes.NewBuffer(cachedData))
-	} else {
-		prxy.ServeHTTP(w, r)
-	}
-}
-
-func hashURL(url string) string {
-	defer elapsed("hashURL")() // <-- The trailing () is the deferred call
-
-	// TODO app name prefix
-	// TODO order query param
-	hasher := md5.New()
-	hasher.Write([]byte(url))
-	return hex.EncodeToString(hasher.Sum(nil))
-}
-
-func checkCache(url string) []byte {
-	defer elapsed("checkCache")()
-	var responseData []byte
-	responseData = repo.Get(url, responseData)
-	return responseData
-}
-
-func returnResponse(cachedData []byte, conn net.Conn) {
-	conn.Write(httpResponseStart)
-	responseLength := len(cachedData)
-	conn.Write([]byte(strconv.Itoa(responseLength)))
-	conn.Write([]byte("\r\n"))
-	conn.Write([]byte("\r\n"))
-	conn.Write(cachedData)
-	conn.Close()
-}
-
-func readURL(reader io.Reader) string {
-	sc := bufio.NewScanner(reader)
-	sc.Scan()
-	uri := sc.Text()
-	return strings.Split(uri, " ")[1]
-}
-
-type ResponseWriter struct {
-	hasCacheHeader bool
-	ResponseBody   bytes.Buffer
-}
-
-func (rw *ResponseWriter) Write(response []byte) (int, error) {
-	rw.ResponseBody.Write(response)
-	return len(response), nil
-}
-
-func proxy(conn net.Conn, request io.Reader, dataChannel chan ([]byte)) {
-	backend, err := net.Dial("tcp", "localhost:8080")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	go func() {
-		io.Copy(backend, request)
-	}()
-
-	go func() {
-		teeReader := io.TeeReader(backend, conn)
-
-		r, err := http.ReadResponse(bufio.NewReader(teeReader), nil)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		if r.Header.Get("Cache-TTL") == "300" {
-			btes, _ := ioutil.ReadAll(r.Body)
-
-			if err == nil {
-				dataChannel <- btes
-			} else {
-				fmt.Println(err)
-			}
-		}
-
-		backend.Close()
-		conn.Close()
-	}()
+	cacheServer.Start()
 }
---
+++ pkg/server/server.go
@@ -0,0 +1,127 @@
+package server
+
+import (
+	"bytes"
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
+
+	"github.com/Trendyol/sidecache/pkg/cache"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.uber.org/zap"
+)
+
+type CacheServer struct {
+	Repo    cache.CacheRepository
+	Proxy   *httputil.ReverseProxy
+	Counter prometheus.Counter
+	Logger  *zap.Logger
+}
+
+func NewServer(repo cache.CacheRepository, proxy *httputil.ReverseProxy, counter prometheus.Counter, logger *zap.Logger) *CacheServer {
+	return &CacheServer{
+		Repo:    repo,
+		Proxy:   proxy,
+		Counter: counter,
+		Logger:  logger,
+	}
+}
+
+func (server CacheServer) Start() {
+	server.Proxy.ModifyResponse = func(r *http.Response) error {
+		defer server.elapsed("ModifyResponse")()
+		//if r.Header.Get("Cache-TTL") == "300" {
+
+		b, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return err
+		}
+
+		go func(reqUrl *url.URL, data []byte) {
+			hashedURL := server.HashURL(server.ReorderQueryString(reqUrl))
+			server.Repo.SetKey(hashedURL, data, 0)
+		}(r.Request.URL, b)
+
+		err = r.Body.Close()
+		if err != nil {
+			return err
+		}
+
+		body := ioutil.NopCloser(bytes.NewReader(b))
+		r.Body = body
+		//}
+
+		return nil
+	}
+
+	http.HandleFunc("/", server.CacheHandler)
+	http.Handle("/metrics", promhttp.Handler())
+
+	server.Logger.Fatal("Error while starting server: ", zap.Error(http.ListenAndServe(":9191", nil)))
+}
+
+func (server CacheServer) elapsed(methodName string) func() {
+	start := time.Now()
+	return func() {
+		server.Logger.Info("",
+			zap.String("MethodName", methodName),
+			zap.Int64("ElapsedTime in MS", time.Since(start).Milliseconds()),
+		)
+	}
+}
+
+func (server CacheServer) CacheHandler(w http.ResponseWriter, r *http.Request) {
+	defer server.elapsed("CacheHandler")() // <-- The trailing () is the deferred call
+
+	defer func() {
+		if rec := recover(); rec != nil {
+			var err error
+			switch x := rec.(type) {
+			case string:
+				err = errors.New(x)
+			case error:
+				err = x
+			default:
+				err = errors.New("Unknown panic")
+			}
+
+			server.Logger.Info("Recovered from panic", zap.Error(err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}()
+
+	hashedURL := server.HashURL(server.ReorderQueryString(r.URL))
+	cachedData := server.CheckCache(hashedURL)
+
+	if cachedData != nil {
+		server.Logger.Info("Cache found")
+		w.Header().Add("X-Cache-Response-For", r.URL.String())
+		io.Copy(w, bytes.NewBuffer(cachedData))
+		server.Counter.Inc()
+	} else {
+		server.Proxy.ServeHTTP(w, r)
+	}
+}
+
+func (server CacheServer) HashURL(url string) string {
+	// TODO app name prefix
+	hasher := md5.New()
+	hasher.Write([]byte(url))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func (server CacheServer) CheckCache(url string) []byte {
+	defer server.elapsed("checkCache")()
+	return server.Repo.Get(url)
+}
+
+func (server CacheServer) ReorderQueryString(url *url.URL) string {
+	return url.Path + "?" + url.Query().Encode()
+}
