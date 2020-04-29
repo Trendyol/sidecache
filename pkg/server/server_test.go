51407d240c0d46d3d250260211cca40020e6cfe5
Haluk Emre Savcı <emre.savci@trendyol.com>
2020-04-30 01:29:23 +0300

    - add unit tests for no cache and proxy

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -3,11 +3,22 @@ package server_test
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"sync"
+	"time"
 
+	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/prometheus/client_golang/prometheus"
+	"go.uber.org/zap"
 
+	"github.com/Trendyol/sidecache/pkg/cache"
 	"github.com/Trendyol/sidecache/pkg/server"
 )
 
@@ -49,5 +60,103 @@ var _ = Describe("Server", func() {
 
 	It("should hash url", func() {
 		Expect(expectedHash).To(Equal(actualHash))
+	})
+})
+
+var _ = Describe("Server", func() {
+
+	var cacheServer *server.CacheServer
+	stopChan := make(chan (int))
+	var repo *cache.MockCacheRepository
+
+	var once sync.Once
+	var afterAllCount int
+
+	BeforeEach(func() {
+		once.Do(func() {
+
+			ctrl := gomock.NewController(GinkgoT())
+			repo = cache.NewMockCacheRepository(ctrl)
+			apiUrl, _ := url.Parse("http://localhost:8080/")
+			proxy := httputil.NewSingleHostReverseProxy(apiUrl)
+			logger, _ := zap.NewProduction()
+			counter := prometheus.NewCounter(
+				prometheus.CounterOpts{
+					Namespace: "sidecache",
+					Name:      "cache_hit_counter",
+					Help:      "This is my counter",
+				})
+
+			cacheServer = server.NewServer(repo, proxy, counter, logger)
+			go cacheServer.Start(stopChan)
+
+			time.Sleep(5 * time.Second)
+		})
+	})
+
+	AfterEach(func() {
+		afterAllCount++
+		fmt.Println(afterAllCount)
+		if afterAllCount == 3 {
+			stopChan <- 1
+		}
+	})
+
+	It("should return cached response", func() {
+		repo.
+			EXPECT().
+			Get(gomock.Any()).
+			Return([]byte("{'name':'emre'}"))
+
+		resp, _ := http.Get("http://localhost:9191/api?name=emre&year=2020")
+		respBody, _ := ioutil.ReadAll(resp.Body)
+
+		Expect(string(respBody)).To(Equal("{'name':'emre'}"))
+	})
+
+	It("should return proxy response", func() {
+		repo.
+			EXPECT().
+			Get(gomock.Any()).
+			Return(nil)
+
+		repo.
+			EXPECT().
+			SetKey(gomock.Any(), gomock.Any(), gomock.Any()).
+			Times(1)
+
+		resp, _ := http.Get("http://localhost:9191/api?name=emre&year=2020")
+		respBody, _ := ioutil.ReadAll(resp.Body)
+
+		actual := make(map[string]string)
+
+		json.Unmarshal(respBody, &actual)
+
+		Expect(actual["Email"]).To(Equal("emre.savci@trendyol.com"))
+	})
+
+	It("should return proxy response when X-No-Cache header exists on request", func() {
+		repo.
+			EXPECT().
+			Get(gomock.Any()).
+			Return(nil)
+
+		repo.
+			EXPECT().
+			SetKey(gomock.Any(), gomock.Any(), gomock.Any()).
+			Times(1)
+
+		client := &http.Client{}
+		req, _ := http.NewRequest("GET", "http://localhost:9191/api?name=emre&year=2020", nil)
+		req.Header.Add("X-No-Cache", "true")
+		resp, _ := client.Do(req)
+
+		respBody, _ := ioutil.ReadAll(resp.Body)
+
+		actual := make(map[string]string)
+
+		json.Unmarshal(respBody, &actual)
+
+		Expect(actual["Email"]).To(Equal("emre.savci@trendyol.com"))
 	})
 })
