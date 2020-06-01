49f2c5c578be4552444b0e664c8b8e432870b4f9
Haluk Emre Savcı <emre.savci@trendyol.com>
2020-06-01 13:14:36 +0300

    - add logging for port and prefix info

--- cmd/sidecache/main.go
+++ cmd/sidecache/main.go
@@ -37,6 +37,7 @@ func main() {
 	couchbaseRepo := cache.NewCouchbaseRepository(logger)
 
 	mainContainerPort := os.Getenv("MAIN_CONTAINER_PORT")
+	logger.Info("Main container port", zap.String("port", mainContainerPort))
 	mainContainerURL, err := url.Parse("http://127.0.0.1:" + mainContainerPort)
 	if err != nil {
 		logger.Error("Error occurred on url.Parse", zap.Error(err))
@@ -44,6 +45,7 @@ func main() {
 
 	proxy := httputil.NewSingleHostReverseProxy(mainContainerURL)
 	cacheServer := server.NewServer(couchbaseRepo, proxy, counter, logger)
+	logger.Info("Cache key prefix", zap.String("prefix", cacheServer.CacheKeyPrefix))
 
 	stopChan := make(chan int)
 	cacheServer.Start(stopChan)
